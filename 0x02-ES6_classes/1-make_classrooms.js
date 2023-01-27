dd311f6e22217a92eb04a80fe704d8defb6818b9
oolufolabii <otitoola.olufolabi@gmail.com>
2023-01-27 05:57:03 +0100

    ES6 Classes Tasks

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from "./0-classroom";
+
+/**
+ * Create an array of {@link ClassRoom}s with custom size
+ * @returns an array of {@link ClassRoom}s
+ */
+
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
