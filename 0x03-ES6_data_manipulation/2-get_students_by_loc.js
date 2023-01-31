4a51a8fdc2ca90a17453d514a0f6a4141135b259
oolufolabii <otitoola.olufolabi@gmail.com>
2023-01-31 20:26:38 +0100

    Tasks

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+/**
+ * Retrieves students in a given location.
+ * @returns an array of objects who are located in a specific city.
+*/
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
