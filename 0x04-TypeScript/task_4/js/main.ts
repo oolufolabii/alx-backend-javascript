5e8af5dcd067adbc93a92c32e0a7ee7996add359
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-02 06:37:58 +0100

    tasks Task

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,9 +2,9 @@ export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-  firstName: 'Dennis',
-  lastName: 'Ritchie',
-  experienceTeachingC: 10,
+  firstName: 'Garvin',
+  lastName: 'Ricketts',
+  experienceTeachingC: 17,
 };
 
 console.log('C++');
