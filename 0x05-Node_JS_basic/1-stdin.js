f49a39855511bf8129ff23d8ab6709da96131a9e
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-21 00:33:08 +0100

    Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const string = process.stdin.read();
+  const string = process.stdin.read();
 
-	if (string) {
-		process.stdout.write(`Your name is: ${string}`);
-	}
+  if (string) {
+    process.stdout.write(`Your name is: ${string}`);
+  }
 });
+
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
