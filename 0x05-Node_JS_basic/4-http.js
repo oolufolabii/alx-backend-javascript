11fcc9c208f369f122427148599b6c5f11817444
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-21 00:34:26 +0100

    Node JS

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,20 +2,19 @@ const http = require('http');
 
 const PORT = 1245;
 const HOST = 'localhost';
-
 const app = http.createServer();
 
-app.on('request', (_, parm) => {
-	const responseText = 'Hello Holberton School!';
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
 
-	parm.setHeader('Content-Type', 'text/plain');
-	parm.setHeader('Content-Length', responseText.length);
-	parm.statusCode = 200;
-	parm.write(Buffer.from(responseText));
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
 app.listen(PORT, HOST, () => {
-	process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
