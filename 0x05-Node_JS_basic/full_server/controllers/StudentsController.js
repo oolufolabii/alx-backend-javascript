5f4d3a13414e3783a7b62c6cd56b1f146e2aef0d
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-21 20:21:38 +0100

    Node Server

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,64 +3,64 @@ import readDatabase from '../utils';
 const VALID_MAJORS = ['CS', 'SWE'];
 
 class StudentsController {
-	static getAllStudents(request, response) {
-		const argPath = process.argv.length > 2 ? process.argv[2] : '';
+  static getAllStudents(request, response) {
+    const argPath = process.argv.length > 2 ? process.argv[2] : '';
 
-		readDatabase(argPath)
-			.then((studGroup) => {
-				const presponse = ['This is the list of our students'];
-				// A comparison function for ordering a list of strings in ascending
-				// order by alphabetic order and case insensitive
-				const cmpFxn = (a, b) => {
-					if (a[0].toLowerCase() < b[0].toLowerCase()) {
-						return -1;
-					}
-					if (a[0].toLowerCase() > b[0].toLowerCase()) {
-						return 1;
-					}
-					return 0;
-				};
+    readDatabase(argPath)
+      .then((studGroup) => {
+        const presponse = ['This is the list of our students'];
+        // A comparison function for ordering a list of strings in ascending
+        // order by alphabetic order and case insensitive
+        const cmpFxn = (a, b) => {
+          if (a[0].toLowerCase() < b[0].toLowerCase()) {
+            return -1;
+          }
+          if (a[0].toLowerCase() > b[0].toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        };
 
-				for (const [field, group] of Object.entries(studGroup).sort(cmpFxn)) {
-					presponse.push([
-						`Number of students in ${field}: ${group.length}.`,
-						'List:',
-						group.map((student) => student.firstname).join(', '),
-					].join(' '));
-				}
-				response.status(200).send(presponse.join('\n'));
-			})
-			.catch((err) => {
-				response
-					.status(500)
-					.send(err instanceof Error ? err.message : err.toString());
-			});
-	}
+        for (const [field, group] of Object.entries(studGroup).sort(cmpFxn)) {
+          presponse.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        response.status(200).send(presponse.join('\n'));
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
+  }
 
-	static getAllStudentsByMajor(request, response) {
-		const argPath = process.argv.length > 2 ? process.argv[2] : '';
-		const { major } = request.params;
+  static getAllStudentsByMajor(request, response) {
+    const argPath = process.argv.length > 2 ? process.argv[2] : '';
+    const { major } = request.params;
 
-		if (!VALID_MAJORS.includes(major)) {
-			response.status(500).send('Major parameter must be CS or SWE');
-			return;
-		}
-		readDatabase(argPath)
-			.then((studGroup) => {
-				let responseText = '';
+    if (!VALID_MAJORS.includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+    readDatabase(argPath)
+      .then((studGroup) => {
+        let responseText = '';
 
-				if (Object.keys(studGroup).includes(major)) {
-					const group = studGroup[major];
-					responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
-				}
-				response.status(200).send(responseText);
-			})
-			.catch((err) => {
-				response
-					.status(500)
-					.send(err instanceof Error ? err.message : err.toString());
-			});
-	}
+        if (Object.keys(studGroup).includes(major)) {
+          const group = studGroup[major];
+          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
+        }
+        response.status(200).send(responseText);
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
+  }
 }
 
 export default StudentsController;
--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,9 +2,9 @@ import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 const linkRoutes = (app) => {
-	app.get('/', AppController.getHomepage);
-	app.get('/students', StudentsController.getAllStudents);
-	app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
 };
 
 export default linkRoutes;
